d7253c8801dc49b328f4808ce969ab54c13142eb
Renato Adorno <renattoadorno@gmail.com>
2024-10-03 14:38:44 -0300

    🎉 initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { peerDependencies } from "./package.json";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "vite-react-ts-button",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["cjs", "es"]
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)]
+    },
+    sourcemap: true,
+    emptyOutDir: true
+  },
+  plugins: [dts()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
